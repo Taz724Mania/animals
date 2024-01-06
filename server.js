fd1f8691aa46c400d4dd660e3c465ffc1c480fee
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-01-06 16:47:27 -0500

    actually connected. server is live

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
-//Dependencies
+//Deps
 require("dotenv").config()
 const express = require("express")
 const morgan = require("morgan")
-const mongoose = require("mongoose")
 const methodOverride = require("method-override")
+const mongoose = require("mongoose")
 
 
+
+//database connect
+const {DATABASE_URL, SECRET, PORT} = process.env
+
+mongoose.connection
+.on("open", () => console.log("Connected to Mongoose"))
+.on("close", () => console.log("Disconnected from Mongoose"))
+.on("error", (error) => console.log(error))
+
+//app object
+const app = express()
+
+//routes
+app.get("/", (req, res) => {
+    res.send("They were, all of them, deceived. For another ring was made.")
+})
+
+
+//server listener
+app.listen(PORT, () => {
+    console.log(`The one ring to rule them all in ${PORT}`)
+})

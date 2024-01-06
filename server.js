2800512fed3e6f949e19443737e2d6888f7c30be
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-01-06 13:53:43 -0500

    connection established

---
+++ server.js
@@ -0,0 +1,8 @@
+//Dependencies
+require("dotenv").config()
+const express = require("express")
+const morgan = require("morgan")
+const mongoose = require("mongoose")
+const methodOverride = require("method-override")
+
+
